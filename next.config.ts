2bd887a8a5dd1bf2ef558744847c37b23589cc0b
Dimitri Pisarev <dimitri@processingpartners.com>
2025-09-14 20:09:51 +0200

    fix: update next.config.ts to include output and trailingSlash settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     formats: ['image/webp', 'image/avif'],
     qualities: [50, 75, 85, 90, 95, 100],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
